bc350d43d3591d591e3b1df65705ec83f0438d34
uvsq21916099 <thomas.joly@ens.uvsq.fr>
2023-10-03 11:56:42 +0200

    debut ajout des attributs

--- Fraction.java
+++ Fraction.java
@@ -1,4 +1,28 @@
 public class Fraction {
+    int numerateur;
+    int denominateur;
+
+    Fraction(int numerateur,int denominateur) {
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+    Fraction(int numerateur) {
+        this.numerateur = numerateur;
+        this.denominateur = 1;
+    }
+    Fraction() {
+        this.numerateur = 0;
+        this.denominateur = 1;
+    }
+
+    public int getNumerateur() {
+        return this.numerateur;
+    }
+
+    public int getDenominateur() {
+        return this.denominateur;
+    }
+
     public static String quisuije(){
         return "Je suis une fraction";
     }
--- Main.java
+++ Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args){
         String jesui = Fraction.quisuije();
         System.out.println(jesui);
+
+        final Fraction ZERO = new Fraction();
+        final Fraction UN = new Fraction(1);
+
         }
 }
